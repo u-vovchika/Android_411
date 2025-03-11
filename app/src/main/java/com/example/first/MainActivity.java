68679a6a4a8c14ffc2a08b4b8d8f297d09eeaa9e
u_vovchika <u-vovchika116@yandex.ru>
2025-03-12 00:25:14 +0300

    DZ_Nizam_third_layout

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -13,8 +13,14 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textSample;
-    private Button secondButton;
+    //private TextView textSample;
+    //private Button secondButton;
+
+    private TextView textSampleDz;
+    private TextView textSampleDz2;
+    private TextView textSampleDz3;
+    private Button thirdButtonDz;
+    private Button fourthButtonDz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +33,58 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.table_layout);
         //setContentView(R.layout.frame_layout);
         //setContentView(R.layout.scroll_view);
-        setContentView(R.layout.third_layout);
+        //setContentView(R.layout.third_layout);
+        setContentView(R.layout.third_layout_dz);
 
-        textSample = findViewById(R.id.textSample);
-        secondButton = findViewById(R.id.secondButton);
+        textSampleDz = findViewById(R.id.textSampleDz);
 
-        secondButton.setOnClickListener(new View.OnClickListener() {
+
+
+        textSampleDz2 = findViewById(R.id.textSampleDz2);
+        textSampleDz3 = findViewById(R.id.textSampleDz3);
+        thirdButtonDz = findViewById(R.id.thirdButtonDz);
+
+        thirdButtonDz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textSample.setText("Новое приветствие");
-                textSample.setTextSize(40);
+                textSampleDz2.setText("Дай денег !!!");
+                textSampleDz3.setText("Нажми Кнопку номер 4");
             }
         });
+
+        fourthButtonDz = findViewById(R.id.fourthButtonDz);
+
+        fourthButtonDz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textSampleDz2.setText("А денег нет ((((");
+                textSampleDz3.setText("Нажми Кнопку номер 3");
+            }
+        });
+
+
+
+//        textSample = findViewById(R.id.textSample);
+//        secondButton = findViewById(R.id.secondButton);
+//
+//        secondButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                textSample.setText("Новое приветствие");
+//                textSample.setTextSize(40);
+//            }
+//        });
     }
 
-    public void sayHello(View view){
-        textSample.setText("Привет");
-        textSample.setTextSize(20);
+    public void sayhelloDz(View view){
+        textSampleDz.setText("Привет Мир");
     }
+    public void sayhelloDz2(View view){
+        textSampleDz.setText("Hello World");
+    }
+//    public void sayHello(View view){
+//        textSample.setText("Привет");
+//        textSample.setTextSize(20);
+//    }
 
 }
